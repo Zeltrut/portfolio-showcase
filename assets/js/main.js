ee15391dcb4555e08d6f4abb9e19076d28ca28a0
Justin Lam <90014114+Zeltrut@users.noreply.github.com>
2024-12-16 22:21:41 -0800

    Full revision with added folders
    Fully finished and refurbished revision of the portfolio website to better organize and add to scalability.

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    if (loadingScreen) loadingScreen.classList.add('hidden');
+});
